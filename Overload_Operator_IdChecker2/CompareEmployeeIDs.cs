e6a58de59deb044d9329c0fc286741738929fb3c
LaurieSue <pink.bee.me@outlook.com>
2020-11-08 15:02:57 -0600

    fix stack overflow error exception by changing the one object to two objects and comparing the ID property instead of the variables from the user input

--- Overload_Operator_IdChecker2/CompareEmployeeIDs.cs
+++ Overload_Operator_IdChecker2/CompareEmployeeIDs.cs
@@ -10,35 +10,30 @@ namespace Overload_Operator_IdChecker2
     class CompareEmployeeIDs
 
     {
-        public int ID_1 { get; set; }
-        public int ID_2 { get; set; }
+        public int ID { get; set; }
+     
     
         public CompareEmployeeIDs()
         {
 
         }
-        public CompareEmployeeIDs(int id_1, int id_2)
-        {
-            ID_1 = id_1;
-            ID_2 = id_2;
-        }
 
-        public static bool operator ==(CompareEmployeeIDs id_1, CompareEmployeeIDs id_2)
+        public static bool operator == ( CompareEmployeeIDs CompareIDs_1, CompareEmployeeIDs CompareIDs_2)
         {
             bool duplicate = false;
 
-            if (id_1 == id_2)
+            if ( CompareIDs_1.ID == CompareIDs_2.ID)
             {
                 duplicate = true;
             }
             return duplicate;
         }
 
-        public static bool operator !=(CompareEmployeeIDs id_1, CompareEmployeeIDs id_2)
+        public static bool operator !=(CompareEmployeeIDs CompareIDs_1, CompareEmployeeIDs CompareIDs_2)
         {
             bool notDuplicate = false;
 
-            if (id_1 != id_2)
+            if (CompareIDs_1.ID != CompareIDs_2.ID)
             {
                 notDuplicate = true;
 
--- Overload_Operator_IdChecker2/Program.cs
+++ Overload_Operator_IdChecker2/Program.cs
@@ -10,21 +10,27 @@ namespace Overload_Operator_IdChecker2
     {
         static void Main(string[] args)
         {
+            CompareEmployeeIDs CompareIDs_1 = new CompareEmployeeIDs();
+            CompareEmployeeIDs CompareIDs_2 = new CompareEmployeeIDs();
+
             Console.WriteLine("Enter First Employee ID#:");
-            int id_1 = Convert.ToInt32(Console.ReadLine());
+            CompareIDs_1.ID = Convert.ToInt32(Console.ReadLine());
           
 
             Console.WriteLine("Enter Second Employee ID#:");
-            int id_2 = Convert.ToInt32(Console.ReadLine());
+            CompareIDs_2.ID = Convert.ToInt32(Console.ReadLine());
 
 
 
 
-            CompareEmployeeIDs CompareIDs = new CompareEmployeeIDs(id_1, id_2);
+    
 
-            
-            Console.WriteLine("Are the Employee ID's Equal:");
-            Console.WriteLine(id_1 == id_2);
+
+            Console.WriteLine(" Are the Employee IDs Equal:");
+            Console.WriteLine(CompareIDs_1.ID == CompareIDs_2.ID);
+            Console.WriteLine("\n Are the Employee IDs Not Equal:");
+            Console.WriteLine(CompareIDs_1.ID != CompareIDs_2.ID);
+            Console.WriteLine("\n If Employee IDs are EQUAL this means they are DUPLICATE ENTRIES \n If Employees are NOT EQUAL, this means there is no detection of duplicate entry");
             Console.ReadLine();
 
 
