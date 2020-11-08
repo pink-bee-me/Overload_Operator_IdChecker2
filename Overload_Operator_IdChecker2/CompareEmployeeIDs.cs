68c197ac5646158a38564e2255c5e323326ac7f7
LaurieSue <pink.bee.me@outlook.com>
2020-11-08 03:26:42 -0600

    Add project files.

---
+++ Overload_Operator_IdChecker2/CompareEmployeeIDs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator_IdChecker2
+{
+    class CompareEmployeeIDs
+
+    {
+        public int ID_1 { get; set; }
+        public int ID_2 { get; set; }
+    
+        public CompareEmployeeIDs()
+        {
+
+        }
+        public CompareEmployeeIDs(int id_1, int id_2)
+        {
+            ID_1 = id_1;
+            ID_2 = id_2;
+        }
+
+        public static bool operator ==(CompareEmployeeIDs id_1, CompareEmployeeIDs id_2)
+        {
+            bool duplicate = false;
+
+            if (id_1 == id_2)
+            {
+                duplicate = true;
+            }
+            return duplicate;
+        }
+
+        public static bool operator !=(CompareEmployeeIDs id_1, CompareEmployeeIDs id_2)
+        {
+            bool notDuplicate = false;
+
+            if (id_1 != id_2)
+            {
+                notDuplicate = true;
+
+            }
+            return notDuplicate;
+        }
+
+    }
+}
---
+++ Overload_Operator_IdChecker2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator_IdChecker2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Employee ID#:");
+            int id_1 = Convert.ToInt32(Console.ReadLine());
+          
+
+            Console.WriteLine("Enter Second Employee ID#:");
+            int id_2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            CompareEmployeeIDs CompareIDs = new CompareEmployeeIDs(id_1, id_2);
+
+            
+            Console.WriteLine("Are the Employee ID's Equal:");
+            Console.WriteLine(id_1 == id_2);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
